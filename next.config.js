62dc7eafbef590e02b3c38f5907b1ccb884ec8c9
Chetan717 <chaitanyachaudhari717@gmail.com>
2024-04-26 12:38:02 +0530

    rtr

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = {
   darkMode: "class",
   plugins: [nextui()],
   env: {
-    GRAPHQL_SERVER: "http://localhost:8000/graph",
+    GRAPHQL_SERVER: "https://serverelectronic.vercel.app/graph",
     SHIPPING: "Pays To Our Delivery Partner",
     Discount: "10"
   }
